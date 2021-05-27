db92b246b6c0e7e6f7829a0aaaa42026e5cb86e2
MercavaSP <staspip@gmail.com>
2021-05-27 12:26:38 +0300

    added some variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,13 @@
+{
+    const money = 500;
+    let income = 600;
+    let addExpenses = 300;
+    const deposit = 50;
+    let mission = 'buy car';
+    let period = '25 may';
+ 
 
+    alert('hello js');
 
-money
-income
-addExpenses
-deposit
-mission
-period
-
-
-
-
-
-
-
-alert('hello js');
-
-console.log('privet JS');
+    console.log('privet JS');
+}
