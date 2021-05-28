2b4d07e24f337a2cedf30029475530c4db28653b
MercavaSP <staspip@gmail.com>
2021-05-28 11:44:45 +0300

    added lesson2 answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,25 @@
 {
     const money = 500;
-    let income = 600;
-    let addExpenses = 300;
-    const deposit = 50;
-    let mission = 'buy car';
-    let period = '25 may';
- 
+    let income = 'Car wash';
+    let addExpenses = 'Eat, Drink, Sleep';
+    let deposit = true;
+    const mission = 63000;
+    const period = 5;
+    let budgetDay = money / 30;
 
-    alert('hello js');
 
-    console.log('privet JS');
+    console.log(typeof(money));
+    console.log(typeof(income));
+    console.log(typeof(deposit));
+
+    console.log(addExpenses.length);
+    
+    console.log('Период равен: ' + period + ' месяцев. ' + 'Цель заработать ' + mission +' евро');
+
+    console.log(addExpenses.toLowerCase().split(', '));
+
+    console.log('Доход за месяц: ' + budgetDay);
+    
 }
+
+
