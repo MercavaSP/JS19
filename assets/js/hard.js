c79b5b02da49779dd29ecd2fcdb8a84e6c6a653a
MercavaSP <staspip@gmail.com>
2021-05-28 12:24:16 +0300

    added answers to hard lesson02

---
+++ assets/js/hard.js
@@ -0,0 +1,11 @@
+{
+    let num = 266219;
+   
+
+
+    console.log(num);
+    console.log(num**3);
+    console.log((num**3).toString().substring(0, 2));
+
+    
+}
---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+{
+    const money = 500;
+    let income = 'Car wash';
+    let addExpenses = 'Eat, Drink, Sleep';
+    let deposit = true;
+    const mission = 63000;
+    const period = 5;
+    let budgetDay = money / 30;
+
+
+    // console.log(typeof(money));
+    // console.log(typeof(income));
+    // console.log(typeof(deposit));
+
+    // console.log(addExpenses.length);
+    
+    // console.log('Период равен ' + period + ' месяцев. ' + 'Цель заработать ' + mission +' евро');
+
+    // console.log(addExpenses.toLowerCase().split(', '));
+
+    // console.log('Доход за месяц: ' + budgetDay);
+    
+}
+
+
