efab504d0176749462feacb8f885fce72795ee82
MercavaSP <staspip@gmail.com>
2021-06-01 16:57:55 +0300

    добавлено решение последней задачи

--- assets/js/hard03.js
+++ assets/js/hard03.js
@@ -2,54 +2,46 @@
 
 // Переменная lang может принимать 2 значения: 'ru' 'en'.
 
-    // let userLanguage = prompt('Укажиьте свой язык:');
-    // const weekDaysRu = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресение'];
-    // const weekDaysEn = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    let userLanguage = prompt('Укажиьте свой язык:');
+    const weekDaysRu = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресение'];
+    const weekDaysEn = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
-    // if (userLanguage === 'ru') {
-    //     console.log([weekDaysRu]);
-    // } else if (userLanguage === 'en') {
-    //     console.log([weekDaysEn]);
-    // } else {
-    //     alert('Незнакомый язык');
-    // }
+
+// через IF 
+    if (userLanguage === 'ru') {
+        console.log([weekDaysRu]);
+    } else if (userLanguage === 'en') {
+        console.log([weekDaysEn]);
+    } else {
+        alert('Незнакомый язык');
+    }
 
 
 
 // через switch-case 
-    // switch (userLanguage) {
-    //     case 'ru':
-    //       console.log([weekDaysRu]);  
-    //       break;
-    //     case 'en':
-    //       console.log([weekDaysEn]);
-    //       break;
-    //     default:
-    //       alert('Незнакомый язык');
-    //     //   break;
-    // }
+    switch (userLanguage) {
+        case 'ru':
+          console.log([weekDaysRu]);  
+          break;
+        case 'en':
+          console.log([weekDaysEn]);
+          break;
+        default:
+          alert('Незнакомый язык');
+    }
 
 /* через многомерный массив без ифов и switch. */
 
-    // let namePerson = prompt('Как вас завут: ');
-    let personSex = prompt('Input your sex: ');
-    
 
-    // if (namePerson == 'Артем') {
-    //     console.log('Директор');
-    // } else if (namePerson === 'Максим') {
-    //     console.log('Учител');
-    // } else {
-    //     console.log('Студент');
-    // }
 
-    
 
+/* У нас есть переменная namePerson. Если значение этой переменной “Артем” то вывести в консоль “директор”,
+ если значение “Максим” то вывести в консоль “преподаватель”,
+  с любым другим значением вывести в консоль “студент”
+
+*/
+    let namePerson = prompt('Как вас завут: ');
    
-    // let stName = namePerson == 'Артем' ? 'Директор' : ('Максим' ? 'Учитель' : 'Ученик');
+    let stName = namePerson == 'Артем' ? 'Директор' : ( namePerson =='Максим' ? 'Учитель' : 'Ученик');
+      console.log(stName);
     
-    let www = personSex === 'Man' ? 'you are man' : ('Woman' ? 'You are woman' : 'other');
-
-    // console.log(stName);
-    
-    console.log(www);
