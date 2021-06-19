5b6341514868c3905606e0197e4934a3a66157f0
MercavaSP <staspip@gmail.com>
2021-06-20 02:37:30 +0300

    todo app functionality

---
+++ js/script.js
@@ -0,0 +1,72 @@
+'use strict';
+
+const todoControl = document.querySelector('.todo-control'),
+    headerInput = document.querySelector('.header-input'),
+    todoList = document.querySelector('.todo-list'),
+    todoCompleted = document.querySelector('.todo-completed');
+
+let todoData = JSON.parse(localStorage.getItem('newTodo'));
+    if (todoData === null) {
+  todoData = [];
+    } else {
+        todoData = JSON.parse(localStorage.getItem('newTodo'));
+    }
+
+const render = function(){
+    todoList.textContent = '';
+    todoCompleted.textContent = '';
+    
+
+    todoData.forEach(function(item, i){
+        const li = document.createElement('li');
+        li.classList.add('todo-item');
+
+        li.innerHTML = '<span class="text-todo">' + item.value + '</span>' +
+            '<div class="todo-buttons">' +
+            '<button class="todo-remove"></button>' +
+            '<button class="todo-complete"></button>' +
+            '</div>';
+
+        if (item.completed) {
+            todoCompleted.append(li);
+        } else {
+            todoList.append(li);
+        }
+
+        const btnTodoCompleted = li.querySelector('.todo-complete');
+
+        btnTodoCompleted.addEventListener('click', function() {
+            item.completed = !item.completed;
+            render();
+        });
+        
+        const btnTodoRemuve = li.querySelector('.todo-remove');
+            btnTodoRemuve.addEventListener('click', function() {
+                todoData.splice(i, 1);
+                render();
+            });
+    });
+    headerInput.value = '';
+};
+
+todoControl.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    if (headerInput.value === '') {
+        return;
+    }
+
+
+    const newTodo = {
+        value: headerInput.value,
+        complited: false
+    };
+
+    todoData.push(newTodo);
+
+    localStorage.setItem('newTodo', JSON.stringify(todoData));
+
+    render();
+});
+
+render();
