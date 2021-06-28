322632274a1d3611c6602790dfcbcdcdcca41c1a
MercavaSP <staspip@gmail.com>
2021-06-28 21:58:32 +0300

    added 17 lesson answer

---
+++ script/script.js
@@ -0,0 +1,46 @@
+'use strict';
+
+window.addEventListener("DOMContentLoaded", () => {
+    // таймер
+    function countTimer(deadline) {
+        const timerHours = document.querySelector('#timer-hours');
+        const timerMinutes = document.querySelector('#timer-minutes');
+        const timerSeconds = document.querySelector('#timer-seconds');
+
+        function getTimeRemaning() {
+            const dateStop = new Date(deadline).getTime();
+            const dateNow = new Date().getTime();
+            const timeRemaining = (dateStop - dateNow) / 1000;
+            const seconds = Math.floor(timeRemaining % 60);
+            const minutes = Math.floor((timeRemaining / 60) % 60);
+            const hours = Math.floor(timeRemaining / 60 / 60);
+            return { timeRemaining, hours, minutes, seconds };
+        }
+
+        function formatTime(data) {
+            if (data < 10) {
+                data = '0' + data;
+            }
+            return data;
+        }
+
+        const timerId = setInterval(() => {
+            const timer = getTimeRemaning();
+            timerHours.textContent = formatTime(timer.hours);
+            timerMinutes.textContent = formatTime(timer.minutes);
+            timerSeconds.textContent = formatTime(timer.seconds);
+
+            if (timer.timeRemaining < 0) {
+                clearInterval(timerId);
+                timerHours.textContent = '00';
+                timerMinutes.textContent = '00';
+                timerSeconds.textContent = '00';
+            }
+        }, 1000);
+
+
+    }
+
+    countTimer('06 july 2021');
+
+});
