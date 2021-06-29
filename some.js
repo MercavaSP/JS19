13d879ea5c528798d7496c7e0c887dc4d530e6af
MercavaSP <staspip@gmail.com>
2021-06-29 16:09:52 +0300

    lesson 17 part 2

--- some.js
+++ some.js
@@ -6,7 +6,6 @@ window.addEventListener("DOMContentLoaded", () => {
         const timerHours = document.querySelector('#timer-hours');
         const timerMinutes = document.querySelector('#timer-minutes');
         const timerSeconds = document.querySelector('#timer-seconds');
-        const timerDays = document.querySelector('#days-till-ny');
 
         function getTimeRemaning() {
             const dateStop = new Date(deadline).getTime();
@@ -14,10 +13,10 @@ window.addEventListener("DOMContentLoaded", () => {
             const timeRemaining = (dateStop - dateNow) / 1000;
             const seconds = Math.floor(timeRemaining % 60);
             const minutes = Math.floor((timeRemaining / 60) % 60);
-            // const hours = Math.floor(timeRemaining / 60 / 60);
-            const hours = Math.floor((timeRemaining / 60 / 60) % 24);
-            const day = Math.floor(timeRemaining / 60 / 60 / 24);
-            return { timeRemaining, hours, minutes, seconds, day };
+            const hours = Math.floor(timeRemaining / 60 / 60);
+            // hours = Math.floor((timeRemaining / 60 / 60) % 24),
+            // day = Math.floor(timeRemaining / 60 / 60 / 24);
+            return { timeRemaining, hours, minutes, seconds };
         }
 
         function formatTime(data) {
@@ -29,10 +28,9 @@ window.addEventListener("DOMContentLoaded", () => {
 
         const timerId = setInterval(() => {
             const timer = getTimeRemaning();
-            // timerHours.textContent = formatTime(timer.hours);
+            timerHours.textContent = formatTime(timer.hours);
             timerMinutes.textContent = formatTime(timer.minutes);
             timerSeconds.textContent = formatTime(timer.seconds);
-            timerDays.textContent = formatTime(timer.day);
 
             if (timer.timeRemaining < 0) {
                 clearInterval(timerId);
