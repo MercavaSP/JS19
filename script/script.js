038bf01604f858fa9335960ba525afbf2c215903
MercavaSP <staspip@gmail.com>
2021-06-29 19:35:46 +0300

    added

--- script/script.js
+++ script/script.js
@@ -40,7 +40,70 @@ window.addEventListener("DOMContentLoaded", () => {
 
 
     }
-
     countTimer('06 july 2021');
 
+    //Меню
+    const toggleMenu = () => {
+        const btnMenu = document.querySelector('.menu'),
+            menu = document.querySelector('menu'),
+            closeBtn = document.querySelector('.close-btn'),
+            menuItems = menu.querySelectorAll('ul>li>a');
+        let count = -50,
+            menuMove;
+
+        const handlerAnimate = () => {
+            menuMove = requestAnimationFrame(handlerAnimate);
+            if (count < 0) {
+                count++;
+                menu.style.transform = `translate(${count * 2}%)`;
+            } else {
+                cancelAnimationFrame(menuMove);
+            }
+        };
+
+        const closeMenu = () => {
+            menu.style.transform = `translate(-100%)`;
+            count = -50;
+        };
+
+        let event = () => {
+            if (menu.style.transform && menu.style.transform === `translate(0%)`) {
+                closeMenu();
+            } else {
+                menuMove = requestAnimationFrame(handlerAnimate);
+            }
+        };
+
+        btnMenu.addEventListener('click', event);
+
+        closeBtn.addEventListener('click', closeMenu);
+        menuItems.forEach(elem => elem.addEventListener('click', closeMenu));
+
+    };
+    if (document.documentElement.clientWidth >= 768) {
+        toggleMenu();
+    }
+
+    //popup
+
+    const toglePopUp = () => {
+        const popup = document.querySelector('.popup'),
+            popupBtn = document.querySelectorAll('.popup-btn'),
+            popUpClose = document.querySelector('.popup-close');
+
+        popupBtn.forEach(elem => {
+            elem.addEventListener('click', () => {
+                popup.style.display = 'block';
+            });
+        });
+
+        popUpClose.addEventListener('click', () => {
+            popup.style.display = 'none';
+        });
+    };
+
+    toglePopUp();
+
+
 });
+
